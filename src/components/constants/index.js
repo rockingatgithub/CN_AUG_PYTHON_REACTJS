4e1336dcbdf90fe1dcef1a9cbe3820e2711184f0
singhzrepo <contactwithsid@gmail.com>
2023-06-25 19:08:23 +0530

    Added deployed URL in constants

--- src/components/constants/index.js
+++ src/components/constants/index.js
@@ -1,6 +1,6 @@
+export const BASE_URL='https://panicky-duck-earmuffs.cyclic.app'
 export const STUDENT_SIGNUP_URL=BASE_URL + '/api/v1/student'
 export const ADMIN_SIGNUP_URL=BASE_URL + '/api/v1/admin'
 export const STUDENT_SIGNIN_URL=BASE_URL + '/api/v1/auth/jwt'
 export const ADMIN_SIGNIN_URL=BASE_URL + '/api/v1/admin/auth/jwt'
-export const BASE_URL='https://panicky-duck-earmuffs.cyclic.app'
 export const LOCAL_BASE='http://localhost:8000'
